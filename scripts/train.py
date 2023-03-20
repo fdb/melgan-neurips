c9920c5c0da6c4e327fc1a0320685f9626de8361
Frederik De Bleser <frederik@debleser.be>
2023-03-20 17:12:18 +0100

    Use the more modern 'to' instead of 'cuda()'.
    This also makes it compatible with CPU-only operation.

--- scripts/train.py
+++ scripts/train.py
@@ -13,6 +13,9 @@ import time
 import argparse
 from pathlib import Path
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -59,11 +62,11 @@ def main():
     #######################
     # Load PyTorch Models #
     #######################
-    netG = Generator(args.n_mel_channels, args.ngf, args.n_residual_layers).cuda()
+    netG = Generator(args.n_mel_channels, args.ngf, args.n_residual_layers).to(device)
     netD = Discriminator(
         args.num_D, args.ndf, args.n_layers_D, args.downsamp_factor
-    ).cuda()
-    fft = Audio2Mel(n_mel_channels=args.n_mel_channels).cuda()
+    ).to(device)
+    fft = Audio2Mel(n_mel_channels=args.n_mel_channels).to(device)
 
     print(netG)
     print(netD)
@@ -102,10 +105,10 @@ def main():
     test_voc = []
     test_audio = []
     for i, x_t in enumerate(test_loader):
-        x_t = x_t.cuda()
+        x_t = x_t.to(device)
         s_t = fft(x_t).detach()
 
-        test_voc.append(s_t.cuda())
+        test_voc.append(s_t.to(device))
         test_audio.append(x_t)
 
         audio = x_t.squeeze().cpu()
@@ -125,9 +128,9 @@ def main():
     steps = 0
     for epoch in range(1, args.epochs + 1):
         for iterno, x_t in enumerate(train_loader):
-            x_t = x_t.cuda()
+            x_t = x_t.to(device)
             s_t = fft(x_t).detach()
-            x_pred_t = netG(s_t.cuda())
+            x_pred_t = netG(s_t.to(device))
 
             with torch.no_grad():
                 s_pred_t = fft(x_pred_t.detach())
@@ -136,8 +139,8 @@ def main():
             #######################
             # Train Discriminator #
             #######################
-            D_fake_det = netD(x_pred_t.cuda().detach())
-            D_real = netD(x_t.cuda())
+            D_fake_det = netD(x_pred_t.to(device).detach())
+            D_real = netD(x_t.to(device))
 
             loss_D = 0
             for scale in D_fake_det:
@@ -153,7 +156,7 @@ def main():
             ###################
             # Train Generator #
             ###################
-            D_fake = netD(x_pred_t.cuda())
+            D_fake = netD(x_pred_t.to(device))
 
             loss_G = 0
             for scale in D_fake:
